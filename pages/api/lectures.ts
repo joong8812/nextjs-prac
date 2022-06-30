3926c247332ae5560058ff0769263fcbc996c2a3
jungjaechoi <joong8812@gmail.com>
2022-06-30 17:53:25 +0900

    Feat: fetch data through a custom hook

--- pages/api/lectures.ts
+++ pages/api/lectures.ts
@@ -56,6 +56,27 @@ const lecturesData = {
   ],
 };
 
+const lecturesDataDataScience = {
+  lectureList: [
+    {
+      id: 0,
+      category: mockCategory,
+      title: "Data Science 초격차 패키지 : 한 번에 끝내는 AWS 인프라 구축과 DevOps 운영",
+      tags: mockTags,
+      description: "개발/운영/아키텍트를 아우르는 AWS/DevOps 전 과정 마스터! 41가지 툴을",
+      thumb: "/thumb.jpg",
+      isHot: true,
+      isNew: true,
+    }
+  ],
+};
+
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json(lecturesData);
+  if (req.query.categories === 'Programming') {
+    res.status(200).json(lecturesData);
+  }
+  if (req.query.categories === 'DataScience') {
+    res.status(200).json(lecturesDataDataScience);
+  }
+  
 }
